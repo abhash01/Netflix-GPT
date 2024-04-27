7ba1fb900c87a47af39483fcdd43b44d3ba35fa7
abhash <abhash01@gmail.com>
2024-04-28 01:23:14 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h1>Abhash kumar ranjan</h1>
+      <h1 className="text-sky-400">Abhash kumar ranjan</h1>
     </div>
   );
 }
